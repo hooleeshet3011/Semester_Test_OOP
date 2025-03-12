581d1ff5aaa4c69ce9227a9b30324b8cfb18a7ff
asian dummy <quykhanhnq53@gmail.com>
2025-03-12 22:26:11 +0700

    Update Program.cs

--- Demo_Test/Program.cs
+++ Demo_Test/Program.cs
@@ -9,25 +9,24 @@ namespace Program
         public static void Main()
         {
             int[] A = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
-            int[] B = { 29, 19, 2, 3 }; // last 4 digit 10418.9712
+            int[] B = { 29, 19, 2, 3 }; // last 4 digits 10418.9712
 
-            FileSystem fs = new FileSystem();
+            FileSystem files = new FileSystem();
 
-            // Adding B[0] files
+            // Add B0 files
             for (int i = 0; i < B[0]; i++)
             {
-                fs.Add(new Demo_Test.File($"1119270-{i}", "txt", 100)); // Assume each file contains 100 bytes
+                files.Add(new Demo_Test.File($"1119270-{i}", "txt", 100)); // Assume each file contains 100 bytes
             }
 
-            // Adding a folder that contains B[1] files
+            // Folder that contains B[1] files
             Folder folder1 = new Folder("Folder1");
             for (int i = 0; i < B[1]; i++)
             {
                 folder1.Add(new Demo_Test.File($"1119270-{i}", "txt", 100));
             }
-            fs.Add(folder1);
+            files.Add(folder1);
 
-            // Adding a folder that contains a folder that contains B[2] files
             Folder folder2 = new Folder("Folder2");
             Folder subFolder = new Folder("SubFolder");
             for (int i = 0; i < B[2]; i++)
@@ -35,17 +34,15 @@ namespace Program
                 subFolder.Add(new Demo_Test.File($"1119270-{i}", "txt", 100));
             }
             folder2.Add(subFolder);
-            fs.Add(folder2);
+            files.Add(folder2);
 
-            // Adding B[3] empty folders
+            // Empty folders
             for (int i = 0; i < B[3]; i++)
             {
-                fs.Add(new Folder($"EmptyFolder{i}"));
+                files.Add(new Folder($"EmptyFolder{i}"));
             }
 
-          
-            fs.PrintContents();
+            files.PrintContents();
         }
     }
-
 }
